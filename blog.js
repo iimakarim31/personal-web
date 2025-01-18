081a1f5e062cb65b1559d298e3fe0663358a9a3d
Iim Abdul Karim <iimakarim31@gmail.com>
2025-01-18 22:47:24 +0700

    add blog page, blog form, blog list, and functionality

--- blog.js
+++ blog.js
@@ -34,6 +34,7 @@ function renderBlog() {
   blogListElement.innerHTML = firstBlogContent();
 
   for (let i = 0; i < blogs.length; i++) {
+    let formattedDate = formatDateToWIB(blogs[i].postedAt);
     // menampilkan blogs yang sudah dibuat dengan mengisi form
     console.log(blogs[i]);
 
@@ -53,11 +54,12 @@ function renderBlog() {
                 </a>
               </h1>
               <div class="detail-blog-content">
-                05 Dec 2024 10:30 WIB | ${blogs[i].author}
+                ${formattedDate} | ${blogs[i].author}
               </div>
               <p class="blog-text">
                 ${blogs[i].content}
               </p>
+              <p class="relative-time">${getRelativeTime(blogs[i].postedAt)}</p>
             </div>
           </div>
     `;
@@ -91,4 +93,48 @@ function firstBlogContent() {
             </div>
           </div>
     `;
+}
+
+function formatDateToWIB(date) {
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "Mei",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Okt",
+    "Nov",
+    "Des",
+  ];
+
+  let day = date.getDate().toString().padStart(2, "0");
+  let month = months[date.getMonth()];
+  let year = date.getFullYear();
+
+  let hours = date.getHours().toString().padStart(2, "0");
+  let minutes = date.getMinutes().toString().padStart(2, "0");
+
+  let formattedDate = `${day} ${month} ${year} ${hours}:${minutes} WIB`;
+  return formattedDate;
+}
+
+function getRelativeTime(targetDate) {
+  let now = new Date();
+  let diffInSeconds = Math.floor((now - targetDate) / 1000);
+
+  console.log(diffInSeconds);
+
+  if (diffInSeconds < 60) {
+    return `${diffInSeconds} seconds ago`;
+  }
+
+  let diffInMinutes = Math.floor(diffInSeconds / 60);
+
+  if (diffInMinutes < 60) {
+    return `${diffInMinutes} minutes ago`;
+  }
 }
